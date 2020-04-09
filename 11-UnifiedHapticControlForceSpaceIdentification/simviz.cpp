17ff3076d99fafc05f3bae32d5b98dc99e0e9b87
MargotVulliez <margot.vulliez@univ-poitiers.fr>
2020-04-09 14:58:56 +0200

    3D application to autonomous switch to force control in contact direction (use omega.7 or keybord commands)

---
+++ 11-UnifiedHapticControlForceSpaceIdentification/simviz.cpp
@@ -0,0 +1,518 @@
+// This example tests the haptic device driver and the open-loop bilateral teleoperation controller.
+
+#include "Sai2Model.h"
+#include "Sai2Graphics.h"
+#include "Sai2Simulation.h"
+#include <dynamics3d.h>
+#include "redis/RedisClient.h"
+#include "timer/LoopTimer.h"
+
+#include <GLFW/glfw3.h> //must be loaded after loading opengl/glew
+
+#include "force_sensor/ForceSensorSim.h" // Add force sensor simulation and display classes
+#include "force_sensor/ForceSensorDisplay.h"
+
+#include <iostream>
+#include <string>
+
+#include <signal.h>
+bool fSimulationRunning = false;
+void sighandler(int){fSimulationRunning = false;}
+
+using namespace std;
+using namespace Eigen;
+using namespace chai3d;
+
+const string world_file = "../resources/11-UnifiedHapticControlForceSpaceIdentification/world.urdf";
+const string robot_file = "../resources/11-UnifiedHapticControlForceSpaceIdentification/panda_arm_sim.urdf";
+const string robot_name = "panda";
+const string camera_name = "camera";
+const string link_name = "link7"; //robot end-effector
+// Set sensor frame transform in end-effector frame
+Affine3d sensor_transform_in_link = Affine3d::Identity();
+const Vector3d sensor_pos_in_link = Eigen::Vector3d(0.0,0.0,0.117);
+
+VectorXd f_task = VectorXd::Zero(6);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Vector3d current_mouse_position = Eigen::Vector3d(0.0, 0.01, 0.0);
+int mouse_gripper = 0;
+// flags for keybord commands
+bool fMouseXp = false;
+bool fMouseXn = false;
+bool fMouseYp = false;
+bool fMouseYn = false;
+bool fMouseZp = false;
+bool fMouseZn = false;
+bool fMouseGripper = false;
+int k=0;
+
+// redis keys:
+// - write (from simulation or robot sensors):
+string JOINT_ANGLES_KEY  = "sai2::Sigma7Applications::sensors::q";
+string JOINT_VELOCITIES_KEY  = "sai2::Sigma7Applications::sensors::dq";
+string FORCE_SENSED_KEY = "sai2::Sigma7Applications::sensors::force_task_sensed";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+string MOUSE_POSITION_KEY = "sai2::Sigma7Applications::sensors::current_mouse_position";
+string MOUSE_GRIPPER_KEY = "sai2::Sigma7Applications::sensors::current_mouse_gripper";
+
+string CONTROLLED_CONTACT_DIRECTION_1 = "sai2::Sigma7Applications::sensors::first_direction_controlled_contact_force";
+string CONTROLLED_CONTACT_DIRECTION_2 = "sai2::Sigma7Applications::sensors::second_direction_controlled_contact_force";
+string CONTROLLED_CONTACT_DIRECTION_3 = "sai2::Sigma7Applications::sensors::third_direction_controlled_contact_force";
+
+
+// - read (from haptic device command input):
+string JOINT_TORQUES_COMMANDED_KEY = "sai2::Sigma7Applications::actuators::torque_joint_robot";
+
+RedisClient redis_client;
+
+// create graphics object
+auto graphics = new Sai2Graphics::Sai2Graphics(world_file, true);
+
+// simulation function prototype
+void simulation(Sai2Model::Sai2Model* robot, Simulation::Sai2Simulation* sim);
+void simulation_dummy(Sai2Model::Sai2Model* robot, Simulation::Sai2Simulation* sim);
+
+// callback to print glfw errors
+void glfwError(int error, const char* description);
+
+// callback when a key is pressed
+void keySelect(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+// callback when a mouse button is pressed
+void mouseClick(GLFWwindow* window, int button, int action, int mods);
+
+// flags for scene camera movement
+bool fTransXp = false;
+bool fTransYp = false;
+bool fTransXn = false;
+bool fTransYn = false;
+bool fTransZp = false;
+bool fTransZn = false;
+bool fshowCameraPose = false;
+bool fRotPanTilt = false;
+// flag for enabling/disabling remote task
+bool fOnOffRemote = false;
+
+const bool flag_simulation = true;
+
+int main() {
+	cout << "Loading URDF world model file: " << world_file << endl;
+	cout << "Keyboard Options:" << endl << endl;
+    cout << "[1] - Enable-Disable remote task" << endl;
+
+	// if(!flag_simulation)
+	// {
+	// 	JOINT_TORQUES_COMMANDED_KEY = "sai2::FrankaPanda::Bonnie::actuators::fgc";
+	// 	JOINT_ANGLES_KEY  = "sai2::FrankaPanda::Bonnie::sensors::q";
+	// 	JOINT_VELOCITIES_KEY = "sai2::FrankaPanda::Bonnie::sensors::dq";
+
+	// 	FORCE_SENSED_KEY= "sai2::ATIGamma_Sensor::force_torque";
+	// }
+
+	// start redis client
+	redis_client = RedisClient();
+	redis_client.connect();
+
+	// load graphics scene
+	Vector3d camera_pos, camera_lookat, camera_vertical;
+	graphics->getCameraPose(camera_name, camera_pos, camera_vertical, camera_lookat);
+
+	// // create a camera
+	// auto camera = new cCamera(graphics->_world);
+	// // add the camera to the world
+	// graphics->_world->addChild(camera);
+	// camera->setUseMultipassTransparency(true);
+
+	// load robots
+	auto robot = new Sai2Model::Sai2Model(robot_file, false);
+
+	// load simulation world
+	auto sim = new Simulation::Sai2Simulation(world_file, false);
+	sim->setCollisionRestitution(0);
+	sim->setCoeffFrictionStatic(0.2);
+
+	// read joint positions, velocities, update model
+	sim->getJointPositions(robot_name, robot->_q);
+	sim->getJointVelocities(robot_name, robot->_dq);
+	robot->updateKinematics();
+
+	/*------- Set up visualization -------*/
+	// set up error callback
+	glfwSetErrorCallback(glfwError);
+
+	// initialize GLFW
+	glfwInit();
+
+	// retrieve resolution of computer display and position window accordingly
+	GLFWmonitor* primary = glfwGetPrimaryMonitor();
+	const GLFWvidmode* mode = glfwGetVideoMode(primary);
+
+	// information about computer screen and GLUT display window
+	int screenW = mode->width;
+	int screenH = mode->height;
+	int windowW = 0.8 * screenH;
+	int windowH = 0.5 * screenH;
+	int windowPosY = (screenH - windowH) / 2;
+	int windowPosX = windowPosY;
+
+	// create window and make it current
+	glfwWindowHint(GLFW_VISIBLE, 0);
+	GLFWwindow* window = glfwCreateWindow(windowW, windowH, "SAI2.0 - Sigma7Applications", NULL, NULL);
+	glfwSetWindowPos(window, windowPosX, windowPosY);
+	glfwShowWindow(window);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	// set callbacks
+	glfwSetKeyCallback(window, keySelect);
+	glfwSetMouseButtonCallback(window, mouseClick);
+
+	// cache variables
+	double last_cursorx, last_cursory;
+
+	fSimulationRunning = true;
+	thread sim_thread(simulation_dummy, robot, sim);
+	if(flag_simulation)
+	{
+		// start the simulation thread first
+		thread sim_thread_bis(simulation, robot, sim);
+		sim_thread.swap(sim_thread_bis);
+		sim_thread_bis.join();
+	}
+
+	//auto handler = new cHapticDeviceHandler();
+
+	// while window is open:
+	while (!glfwWindowShouldClose(window))
+	{
+		if(!flag_simulation)
+		{
+			// robot->_q = redis_client.getEigenMatrixJSON(JOINT_ANGLES_KEY);
+			// robot->_dq = redis_client.getEigenMatrixJSON(JOINT_VELOCITIES_KEY);
+			// robot->updateKinematics();
+		}
+
+		// update graphics. this automatically waits for the correct amount of time
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		graphics->updateGraphics(robot_name, robot);
+		graphics->render(camera_name, width, height);
+
+		// swap buffers
+		glfwSwapBuffers(window);
+
+		// wait until all GL commands are completed
+		glFinish();
+
+		// check for any OpenGL errors
+		GLenum err;
+		err = glGetError();
+		assert(err == GL_NO_ERROR);
+
+		// poll for events
+		glfwPollEvents();
+
+		// move scene camera as required
+		// graphics->getCameraPose(camera_name, camera_pos, camera_vertical, camera_lookat);
+		Eigen::Vector3d cam_depth_axis;
+		cam_depth_axis = camera_lookat - camera_pos;
+		cam_depth_axis.normalize();
+		Eigen::Vector3d cam_up_axis;
+		// cam_up_axis = camera_vertical;
+		// cam_up_axis.normalize();
+		cam_up_axis << 0.0, 0.0, 1.0; //TODO: there might be a better way to do this
+		Eigen::Vector3d cam_roll_axis = (camera_lookat - camera_pos).cross(cam_up_axis);
+		cam_roll_axis.normalize();
+		Eigen::Vector3d cam_lookat_axis = camera_lookat;
+		cam_lookat_axis.normalize();
+		if (fTransXp) {
+			camera_pos = camera_pos + 0.01*cam_roll_axis;
+			camera_lookat = camera_lookat + 0.01*cam_roll_axis;
+		}
+		if (fTransXn) {
+			camera_pos = camera_pos - 0.01*cam_roll_axis;
+			camera_lookat = camera_lookat - 0.01*cam_roll_axis;
+		}
+		if (fTransYp) {
+			// camera_pos = camera_pos + 0.05*cam_lookat_axis;
+			camera_pos = camera_pos + 0.01*cam_up_axis;
+			camera_lookat = camera_lookat + 0.01*cam_up_axis;
+		}
+		if (fTransYn) {
+			// camera_pos = camera_pos - 0.05*cam_lookat_axis;
+			camera_pos = camera_pos - 0.01*cam_up_axis;
+			camera_lookat = camera_lookat - 0.01*cam_up_axis;
+		}
+		if (fTransZp) {
+			camera_pos = camera_pos + 0.05*cam_depth_axis;
+			camera_lookat = camera_lookat + 0.05*cam_depth_axis;
+		}
+		if (fTransZn) {
+			camera_pos = camera_pos - 0.05*cam_depth_axis;
+			camera_lookat = camera_lookat - 0.05*cam_depth_axis;
+		}
+		if (fshowCameraPose) {
+			cout << endl;
+			cout << "camera position : " << camera_pos.transpose() << endl;
+			cout << "camera lookat : " << camera_lookat.transpose() << endl;
+			cout << endl;
+		}
+		if (fRotPanTilt) {
+			// get current cursor position
+			double cursorx, cursory;
+			glfwGetCursorPos(window, &cursorx, &cursory);
+			//TODO: might need to re-scale from screen units to physical units
+			double compass = 0.006*(cursorx - last_cursorx);
+			double azimuth = 0.006*(cursory - last_cursory);
+			double radius = (camera_pos - camera_lookat).norm();
+			Eigen::Matrix3d m_tilt; m_tilt = Eigen::AngleAxisd(azimuth, -cam_roll_axis);
+			camera_pos = camera_lookat + m_tilt*(camera_pos - camera_lookat);
+			Eigen::Matrix3d m_pan; m_pan = Eigen::AngleAxisd(compass, -cam_up_axis);
+			camera_pos = camera_lookat + m_pan*(camera_pos - camera_lookat);
+		}
+		graphics->setCameraPose(camera_name, camera_pos, cam_up_axis, camera_lookat);
+		glfwGetCursorPos(window, &last_cursorx, &last_cursory);
+
+
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		if (fMouseXp) {
+			current_mouse_position[0] += 0.0001;
+		}
+		if (fMouseXn) {
+			current_mouse_position[0] -= 0.0001;
+		}
+		if (fMouseYp) {
+			current_mouse_position[1] += 0.0001;
+		}
+		if (fMouseYn) {
+			current_mouse_position[1] -= 0.0001;
+		}
+		if (fMouseZp) {
+			current_mouse_position[2] += 0.0001;
+		}
+		if (fMouseZn) {
+			current_mouse_position[2] -= 0.0001;
+		}
+		if (fMouseGripper || k>=1)
+		{
+			mouse_gripper = 1;
+			k ++;
+		}
+		else
+		{
+			mouse_gripper = 0;
+		}
+		if (k>=20)
+		{
+			k=0;
+		}
+
+
+	}
+
+	// stop simulation
+	fSimulationRunning = false;
+	sim_thread.join();
+
+	// destroy context
+	glfwDestroyWindow(window);
+
+	// terminate
+	glfwTerminate();
+
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+void simulation_dummy(Sai2Model::Sai2Model* robot, Simulation::Sai2Simulation* sim) {}
+
+//------------------------------------------------------------------------------
+void simulation(Sai2Model::Sai2Model* robot, Simulation::Sai2Simulation* sim) {
+
+	Vector3d contact_control_first = Vector3d::Zero();
+	Vector3d contact_control_second = Vector3d::Zero();
+	Vector3d contact_control_third = Vector3d::Zero();
+
+	redis_client.setEigenMatrixJSON(CONTROLLED_CONTACT_DIRECTION_1,contact_control_first);
+	redis_client.setEigenMatrixJSON(CONTROLLED_CONTACT_DIRECTION_2,contact_control_second);
+	redis_client.setEigenMatrixJSON(CONTROLLED_CONTACT_DIRECTION_3,contact_control_third);
+
+
+	// auto fsensor_display = new ForceSensorDisplay(force_sensor, graphics);
+
+	VectorXd command_torques = VectorXd::Zero(robot->dof());
+	redis_client.setEigenMatrixJSON(JOINT_TORQUES_COMMANDED_KEY, command_torques);
+
+	sensor_transform_in_link.translation() = sensor_pos_in_link;
+	//sensor_transform_in_link.linear() = Matrix3d::Identity();
+
+	// Add force sensor to the end-effector
+	auto force_sensor = new ForceSensorSim(robot_name, link_name, sensor_transform_in_link, robot);
+	auto fsensor_display = new ForceSensorDisplay(force_sensor, graphics);
+	Vector3d sensed_force = Vector3d::Zero();
+	Vector3d sensed_moment = Vector3d::Zero();
+	// force_sensor->enableFilter(0.016);
+
+	// create a timer
+	LoopTimer timer;
+	timer.initializeTimer();
+	timer.setLoopFrequency(2000);
+	double last_time = timer.elapsedTime(); //secs
+	bool fTimerDidSleep = true;
+
+	unsigned long long simulation_counter = 0;
+
+	while (fSimulationRunning) {
+		fTimerDidSleep = timer.waitForNextLoop();
+
+		// read arm torques from redis
+		command_torques = redis_client.getEigenMatrixJSON(JOINT_TORQUES_COMMANDED_KEY);
+
+		// read force controlled direction
+		contact_control_first = redis_client.getEigenMatrixJSON(CONTROLLED_CONTACT_DIRECTION_1);
+		contact_control_second = redis_client.getEigenMatrixJSON(CONTROLLED_CONTACT_DIRECTION_2);
+		contact_control_third = redis_client.getEigenMatrixJSON(CONTROLLED_CONTACT_DIRECTION_3);
+
+		sim->setJointTorques(robot_name, command_torques);
+
+		// integrate forward
+		double curr_time = timer.elapsedTime();
+		double loop_dt = curr_time - last_time;
+		sim->integrate(loop_dt);
+
+		// read joint positions, velocities, update model
+		sim->getJointPositions(robot_name, robot->_q);
+		sim->getJointVelocities(robot_name, robot->_dq);
+		robot->updateKinematics();
+
+		// write new robot state to redis
+		redis_client.setEigenMatrixJSON(JOINT_ANGLES_KEY, robot->_q);
+		redis_client.setEigenMatrixJSON(JOINT_VELOCITIES_KEY, robot->_dq);
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		redis_client.setEigenMatrixJSON(MOUSE_POSITION_KEY, current_mouse_position); //if no device use keybord keys
+		redis_client.set(MOUSE_GRIPPER_KEY, to_string(mouse_gripper));
+
+
+		// read end-effector task forces from the force sensor simulation
+		force_sensor->update(sim);
+		force_sensor->getForce(sensed_force);
+		force_sensor->getMoment(sensed_moment);
+		f_task << sensed_force, sensed_moment;
+		// write task force in redis
+		redis_client.setEigenMatrixJSON(FORCE_SENSED_KEY,f_task);
+
+		//update last time
+		last_time = curr_time;
+
+		simulation_counter++;
+	}
+
+	double end_time = timer.elapsedTime();
+	std::cout << "\n";
+	std::cout << "Simulation Loop run time  : " << end_time << " seconds\n";
+	std::cout << "Simulation Loop updates   : " << timer.elapsedCycles() << "\n";
+	std::cout << "Simulation Loop frequency : " << timer.elapsedCycles()/end_time << "Hz\n";
+}
+
+//------------------------------------------------------------------------------
+
+void glfwError(int error, const char* description) {
+	cerr << "GLFW Error: " << description << endl;
+	exit(1);
+}
+
+//------------------------------------------------------------------------------
+
+void keySelect(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	bool set = (action != GLFW_RELEASE);
+	switch(key) {
+		case GLFW_KEY_ESCAPE:
+			// exit application
+			glfwSetWindowShouldClose(window,GL_TRUE);
+			break;
+		case GLFW_KEY_RIGHT:
+			fTransXp = set;
+			break;
+		case GLFW_KEY_LEFT:
+			fTransXn = set;
+			break;
+		case GLFW_KEY_UP:
+			fTransYp = set;
+			break;
+		case GLFW_KEY_DOWN:
+			fTransYn = set;
+			break;
+		case GLFW_KEY_A:
+			fTransZp = set;
+			break;
+		case GLFW_KEY_Z:
+			fTransZn = set;
+			break;
+		case GLFW_KEY_R:
+			fOnOffRemote = set;
+	    break;
+		case GLFW_KEY_S:
+			fshowCameraPose = set;
+			break;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			case GLFW_KEY_U:
+				fMouseXp = set;
+		    break;
+			case GLFW_KEY_J:
+				fMouseXn = set;
+		    break;
+			case GLFW_KEY_I:
+				fMouseYp = set;
+		    break;
+			case GLFW_KEY_K:
+				fMouseYn = set;
+		    break;
+			case GLFW_KEY_O:
+				fMouseZp = set;
+		    break;
+			case GLFW_KEY_L:
+				fMouseZn = set;
+		    break;
+			case GLFW_KEY_X:
+				fMouseGripper = set;
+		    break;
+
+		default:
+			break;
+
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void mouseClick(GLFWwindow* window, int button, int action, int mods) {
+	bool set = (action != GLFW_RELEASE);
+	//TODO: mouse interaction with robot
+	switch (button) {
+		// left click pans and tilts
+		case GLFW_MOUSE_BUTTON_LEFT:
+			fRotPanTilt = set;
+			// NOTE: the code below is recommended but doesn't work well
+			// if (fRotPanTilt) {
+			// 	// lock cursor
+			// 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+			// } else {
+			// 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+			// }
+			break;
+		// if right click: don't handle. this is for menu selection
+		case GLFW_MOUSE_BUTTON_RIGHT:
+			//TODO: menu
+			break;
+		// if middle click: don't handle. doesn't work well on laptops
+		case GLFW_MOUSE_BUTTON_MIDDLE:
+			break;
+		default:
+			break;
+	}
+}
